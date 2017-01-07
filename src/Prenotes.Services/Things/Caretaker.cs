c4f03bfbb1cba76ef17873863690fb12b9a53c36
Matthew Young <matthewdyoung@gmail.com>
2017-01-07 11:54:20 +0100

    Actions, Stores, Things plus room for validation

---
+++ src/Prenotes.Services/Things/Caretaker.cs
@@ -0,0 +1,12 @@
+
+namespace Prenotes.Services.Things {
+
+    public struct Caretaker {
+        // Unique identifier
+        public string email;
+
+        public Caretaker(string email) {
+            this.email = email;
+        }
+    }
+}
