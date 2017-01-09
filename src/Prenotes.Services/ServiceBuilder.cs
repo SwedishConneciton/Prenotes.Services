0b2687d9dc460febc96ece3ffb62fff730cd90dc
Matthew Young <matthewdyoung@gmail.com>
2017-01-09 07:57:04 +0100

    Example of a service builder

--- src/Prenotes.Services/ServiceBuilder.cs
+++ src/Prenotes.Services/ServiceBuilder.cs
@@ -1,7 +1,20 @@
 
+using Neo4j.Driver.V1;
+using Prenotes.Services.Actions;
+using Prenotes.Services.Validation;
+
 namespace Prenotes.Services {
 
     public static class ServiceBuilder {
 
+        public static ICaretakerService CaretakerService(string url, string user, string password) {
+
+            var driver = GraphDatabase.Driver(
+                url, 
+                AuthTokens.Basic(user, password)
+            );
+
+            return new CaretakerValidator(new CaretakerService(driver));
+        }
     }
 }
