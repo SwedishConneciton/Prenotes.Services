a86654700c2e7d7f14ddced94820665bfc21ae8b
Matthew Young <matthewdyoung@gmail.com>
2017-01-13 12:03:05 +0100

    To student for work

---
+++ src/Prenotes.Services/Actions/StartupService.cs
@@ -0,0 +1,20 @@
+
+
+namespace Prenotes.Services.Actions {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class StartupService {
+
+        /// <summary>
+        /// Makes sure that the primary Prenotes organization exists
+        /// and that handshakes have been issued for super users.
+        /// </summary>
+        /// <param name="name">Organization name</param>
+        /// <param name="email">Super users</param>
+        public void Configure(string name, string[] email) {
+
+        }
+    }
+}
